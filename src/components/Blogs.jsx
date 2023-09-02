8d382e0846009b93de5af9c39fdfbb905fa6e67b
unknown <sohailshahidi1435@gmail.com>
2023-09-02 22:29:16 +0430

    Add new News

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import blog1 from '../images/blog1.webp';
+import blog2 from '../images/blog2.webp';
 
 const Blogs = () => {
   const blogs = [
@@ -9,6 +10,12 @@ const Blogs = () => {
       textDetails: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur dolores perspiciatis vero soluta quasi laborum, magni dolor sint quibusdam minima voluptate! Dignissimos eligendi temporibus nesciunt vitae dolor maiores tenetur delectus quaerat, recusandae aliquid et, ab, repudiandae minima voluptate quos sequi.',
       date: 'Sep/01/2023',
     },
+    {
+      image: blog2,
+      blogTitle: 'Our Teachers and Education System',
+      textDetails: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur dolores perspiciatis vero soluta quasi laborum, magni dolor sint quibusdam minima voluptate! Dignissimos eligendi temporibus nesciunt vitae dolor maiores tenetur delectus quaerat, recusandae aliquid et, ab, repudiandae minima voluptate quos sequi.',
+      date: 'Sep/02/2023',
+    },
   ];
   return (
     <section className="blogs">
@@ -16,7 +23,7 @@ const Blogs = () => {
         <section className="blog-container" key={blogs.id}>
           <img src={blog.image} alt={blog.image} className="blog-image" />
           <article className="blog-details">
-            <h2>{blog.blogTitle}</h2>
+            <h3>{blog.blogTitle}</h3>
             <p>{blog.textDetails}</p>
             <p>{blog.date}</p>
           </article>
