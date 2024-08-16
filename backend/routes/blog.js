e45a82804cdf71d7d3a5a663f86214b84141cc22
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-08-16 11:43:00 +0430

    Create blogRoutes

---
+++ backend/routes/blog.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.router
+
+router.get('/', () => {})
+
+router.get('/:id', () => {})
+
+router.post('/', () => {})
+
+router.delete('/:id', () => {})
+
+router.patch('/:id', () => {})
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const blogRoutes = require('./routes/blog')
 const userRoutes = require('./routes/user')
 
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 })
 
 // routes
+app.use('/api/blog', blogRoutes)
 app.use('/api/user', userRoutes)
 
 // connect to db
