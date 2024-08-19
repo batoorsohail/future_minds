62dd2162dc865516f2f70f62f4d1ad787ee19a58
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-08-19 18:57:10 +0430

    Display blogs

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -3,11 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 import { getBlogs, selectAllBlogs } from '../redux/blogs/blogsSlice';
-import blog1 from '../images/blog1.webp';
-import blog2 from '../images/blog2.webp';
 
 const Blogs = () => {
-  const blogs2 = useSelector(selectAllBlogs);
+  const blogs = useSelector(selectAllBlogs);
 
   const dispatch = useDispatch();
 
@@ -15,35 +13,19 @@ const Blogs = () => {
     dispatch(getBlogs())
   }, [dispatch]);
 
-  console.log(blogs2);
-
-  const blogs = [
-    {
-      image: blog1,
-      blogTitle: 'The First Gradated Students of Future Minds',
-      textDetails: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur dolores perspiciatis vero soluta quasi laborum, magni dolor sint quibusdam minima voluptate! Dignissimos eligendi temporibus nesciunt vitae dolor maiores tenetur delectus quaerat, recusandae aliquid et, ab, repudiandae minima voluptate quos sequi.',
-      date: 'Sep/01/2023',
-    },
-    {
-      image: blog2,
-      blogTitle: 'Our Teachers and Education System',
-      textDetails: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur dolores perspiciatis vero soluta quasi laborum, magni dolor sint quibusdam minima voluptate! Dignissimos eligendi temporibus nesciunt vitae dolor maiores tenetur delectus quaerat, recusandae aliquid et, ab, repudiandae minima voluptate quos sequi.',
-      date: 'Sep/02/2023',
-    },
-  ];
   return (
     <section className="blogs">
       {blogs.map((blog) => (
         <section className="blog-container" key={blogs.id}>
-          <img src={blog.image} alt={blog.image} className="blog-image" />
+          {/* <img src={blog.image} alt={blog.image} className="blog-image" /> */}
           <article className="blog-details">
-            <h3 className="blog-title-name">{blog.blogTitle}</h3>
+            <h3 className="blog-title-name">{blog.title}</h3>
             <p className="blog-details-text">
-              {blog.textDetails.slice(0, 100)}
-              {blog.textDetails.length > 100 && '...'}
+              {blog.content.slice(0, 100)}
+              {blog.content.length > 100 && '...'}
             </p>
             <div className="blog-card-bottom">
-              <p className="blog-date">{blog.date}</p>
+              <p className="blog-date">{blog.createdAt}</p>
               <Link to={`blog/${blog.id}`} className="link-to-blog">
                 <p className="link-to-blog-txt">Read More</p>
                 <BsArrowRight className="arrow-icon" />
