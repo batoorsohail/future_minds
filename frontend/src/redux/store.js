8fb2c20e3c54975c9b93eea7317c07769d461f0f
Sohail Batoor <sohailshahidi1435@gmail.com>
2024-09-16 22:07:22 +0430

    Add authSlice.js file

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import blogsReducer from "./blogs/blogsSlice"
+import authReducer from "./auth/authSlice"
 
 const store = configureStore({
   reducer: {
-    blogs: blogsReducer
+    blogs: blogsReducer,
+    auth: authReducer
   }
 })
 
